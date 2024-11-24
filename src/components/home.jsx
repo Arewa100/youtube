caca029f06665f5730c84ae848b926ae9ae0a703
olawhales <ajaditaoreed@gmail.com>
2024-11-25 00:58:30 +0100

    i noticed you've made changes while was working so i want to check if the file is still goona be arrange

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,23 @@
 import style from '../styles/home.module.css'
+import searchBarButton from '../reusable/searchButton';
 
-const Home = ()=> {
+
+function Home (){
     return(
-        <>
         <div className={style.mainWrapper}>
+            <form action='home'>
+                <input 
+                type="text" 
+                name='name'
+                placeholder='search'
+                />
+            </form>
 
+            <div>
+                <searchBarButton style={style.btn} type="submi" textContent="submit"/>
+            </div>
         </div>
-        </>
+
     )
 }
 
--- src/reusable/searchButton.jsx
+++ src/reusable/searchButton.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-const searchBar = ()=> {
+const searchBarButton = (props)=> {
+    const {style , onClick, type , textContent ,}= props;
     return(
-        <>
-        </>
+        
+        <button className="{style}" onClick={onClick}  type={type}>
+            {textContent}
+        </button>
+        
     )
 }
 
-export default searchBar
+export default searchBarButton ; 
+
