ef67e2d01f7fe0940adef3bc788fb5c55beebf8a
Arewa100 <olasorexway@gmail.com>
2024-11-25 02:17:51 +0100

    new update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,24 +1,35 @@
 import style from '../styles/home.module.css'
-import searchBarButton from '../reusable/searchButton';
+import NavBar from './navBar';
 
 
-function Home (){
+const Home = ()=> {
     return(
-        <div className={style.mainWrapper}>
-            <form action='home'>
-                <input 
-                type="text" 
-                name='name'
-                placeholder='search'
-                />
-            </form>
-
-            <div>
-                <searchBarButton style={style.btn} type="submi" textContent="submit"/>
-            </div>
-        </div>
-
+       <>
+        <div className={style.wrapper}>
+         <NavBar/>
+       </div>
+       </>
     )
 }
+
+
+// function Home (){
+//     return(
+//         <div className={style.mainWrapper}>
+//             <form action='home'>
+//                 <input 
+//                 type="text" 
+//                 name='name'
+//                 placeholder='search'
+//                 />
+//             </form>
+
+//             <div>
+//                 <searchBarButton style={style.btn} type="submi" textContent="submit"/>
+//             </div>
+//         </div>
+
+//     )
+// }
 
 export default Home;
